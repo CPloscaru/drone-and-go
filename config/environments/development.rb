7311ca86354290cbdc7b2a6afe1f75bfcd65d852
Calin Ploscaru <ytcognito@gmail.com>
2025-03-04 11:04:40 +0100

    Install and configure devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Add line to config "devise" gem
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
