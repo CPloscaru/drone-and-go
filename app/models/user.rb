1412fcf86a92a3f13c97cc3dd078d5ec5f101764
Calin Ploscaru <ytcognito@gmail.com>
2025-03-04 15:50:29 +0100

    add route create to drones and validations to users (name, last_name)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :drones
   has_many :bookings
+
+  validates :name, presence: true
+  validates :last_name, presence: true
 end
