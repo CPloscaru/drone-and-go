786b6febb180ed51cfc3882e8c980f7fa4bdb9c2
David BELLOT <davidbellot@icloud.com>
2025-03-05 14:12:03 +0100

    afficher les infos des drones ainsi que les reviews avec les étoiles

--- app/controllers/drones_controller.rb
+++ app/controllers/drones_controller.rb
@@ -1,6 +1,17 @@
 class DronesController < ApplicationController
   def index
     @drones = Drone.all
+    if params[:sort_by]
+      case params[:sort_by]
+      when "autonomy_rating"
+        @drones = @drones.order(autonomy_rating: :desc)
+      when "stability_rating"
+        @drones = @drones.order(stability_rating: :desc)
+      when "range_rating"
+        @drones = @drones.order(range_rating: :desc)
+      when "price_for_lease"
+        @drones = @drones.order(price_for_lease: :asc)
+      end
   end
 
   def new
@@ -8,4 +19,5 @@ class DronesController < ApplicationController
 
   def show
   end
+end
 end
